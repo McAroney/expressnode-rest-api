d83b32e6b95780429e0d439beb239f060e438358
Aron Elal <aron.elal@gmail.com>
2020-05-17 14:57:07 +0300

    ...oonkohan pikkusen taukki..?

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-var urlencode = require("urlencode");
 var user = process.env.DB_USER;
 var pass = process.env.DB_PASS;
-var dbUrl = urlencode(
-	"mongodb://" + user + ":" + pass + "@ds155699.mlab.com:55699/products"
-);
+var dbUrl =
+	"mongodb://" + user + ":" + pass + "@ds155699.mlab.com:55699/products";
 
 var productsRouter = require("./routes/products");
 
