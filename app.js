2c64a22f205c49173b105ab29232f2afae63ab4b
Aron Elal <aron.elal@gmail.com>
2020-05-17 16:35:01 +0300

    cors

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(function (req, res, next) {
 	next();
 });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
