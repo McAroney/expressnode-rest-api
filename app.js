e1628f7301a4cce0e6f91e37a29b28ac20ae985e
Aron Elal <aron.elal@gmail.com>
2020-05-17 12:24:26 +0300

    Over simplifying things

--- app.js
+++ app.js
@@ -26,34 +26,9 @@ mongoose
   .then(() => console.log('connection succesful'))
   .catch(err => console.error(err))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: 'true' }))
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/users', usersRouter)
 app.use('/products', productsRouter)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
 
 module.exports = app
--- routes/products.js
+++ routes/products.js
@@ -8,17 +8,11 @@ router.get('/', function (req, res) {
   console.log("entered '.../products/' route..")
   Product.find({}, function (err, products) {
     if (err) res.json({ err: err })
-    res.json({ res: products })
+    res.json(products)
   })
 })
 
 /* GET SINGLE PRODUCT BY ID */
-router.get('/:id', function (req, res) {
-  Product.findById(req.params.id, function (err, post) {
-    if (err) return res.json({ err: err })
-    res.json(post)
-  })
-})
 
 /* SAVE PRODUCT */
 router.post('/', function (req, res) {
@@ -29,19 +23,7 @@ router.post('/', function (req, res) {
 })
 
 /* UPDATE PRODUCT */
-router.put('/:id', function (req, res) {
-  Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) res.json({ err: err })
-    res.json(post)
-  })
-})
 
 /* DELETE PRODUCT */
-router.delete('/:id', function (req, res) {
-  Product.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) res.json({ err: err })
-    res.json(post)
-  })
-})
 
 module.exports = router
