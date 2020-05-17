804ab477d01b18be3d447e212aab9526079c8333
Aron Elal <aron.elal@gmail.com>
2020-05-17 16:17:53 +0300

    Reforming response object

--- routes/products.js
+++ routes/products.js
@@ -1,29 +1,30 @@
-var express = require('express')
-var router = express.Router()
-var mongoose = require('mongoose')
-var Product = require('../models/Product.js')
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+var Product = require("../models/Product.js");
 
 /* GET ALL PRODUCTS */
-router.get('/', function (req, res) {
-  console.log("entered '.../products/' route..")
-  Product.find({}, function (err, products) {
-    if (err) res.json({ err: err })
-    res.json(products)
-  })
-})
+router.get("/", function (req, res) {
+	console.log("entered '.../products/' route..");
+	console.log(req);
+	Product.find({}, function (err, products) {
+		if (err) res.json({ err: err });
+		res.json({ products: products });
+	});
+});
 
 /* GET SINGLE PRODUCT BY ID */
 
 /* SAVE PRODUCT */
-router.post('/', function (req, res) {
-  Product.create(req.body, function (err, post) {
-    if (err) res.json({ err: err })
-    res.json(post)
-  })
-})
+router.post("/", function (req, res) {
+	Product.create(req.body, function (err, post) {
+		if (err) res.json({ err: err });
+		res.json(post);
+	});
+});
 
 /* UPDATE PRODUCT */
 
 /* DELETE PRODUCT */
 
-module.exports = router
+module.exports = router;
