187a41e1fb1798fc67e91d0db24c687952bb01d3
Aron Elal <aron.elal@gmail.com>
2020-05-17 15:18:23 +0300

    Something changed.. Woohoo!

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ mongoose
 	.then(() => console.log("connection succesful"))
 	.catch((err) => console.error(err));
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "true" }));
 
